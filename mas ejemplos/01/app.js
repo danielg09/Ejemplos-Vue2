f99f92ac5da1d32267f5cfad47b8598f6bfbd697
Daniel <dgerardo94@gmail.com>
2019-03-06 20:29:26 -0600

    Se agregan mas ejemplos

---
+++ mas ejemplos/01/app.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo : 'Mis frutas preferidas',
+        frutas : [
+            {nombre: 'Sandia', cantidad: 10},
+            {nombre: 'Melon', cantidad: 0},
+            {nombre: 'Pera', cantidad: 15}
+        ]
+    } 
+
+})
